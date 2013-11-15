187b771ecd2f29e870b7970436286b4c178c5255
Alberto Aguilera <drimer.aaj@gmail.com>
2013-11-15 00:49:01 +0000

    tests/run-tests.sh: Store stdout and stderr of test runs
    It's better to debug failures when we have stdout and stderr of the test
    runs. If they pass, we don't care about them, so we just delete them.

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -15,15 +15,26 @@ for tests_file in $own_dir/tests-*.sh; do
     source $tests_file
 done
 
+# Prepare directory for test results
+all_results_dir="test-results"
+mkdir -p $all_results_dir
+
 # Execute tests
 ret=0
 for t in $(declare -F | awk '/ test_/ {print $3}'); do
-    $t && {
+    test_results_dir="$all_results_dir/$t"
+    mkdir -p $test_results_dir
+    $t >"$test_results_dir/stdout.log" \
+	2>"$test_results_dir/stderr.log"
+    [[ $? == 0 ]] && {
+	rm -r $test_results_dir;
 	[[ "$verbose" == "true" ]] && echo "$t: PASSED";
     } || {
 	[[ "$verbose" == "true" ]] && echo "$t: FAILED";
 	ret=1;
     }
 done
+
+[[ $ret == 0 ]] && rm -r $all_results_dir
 
 exit $ret
